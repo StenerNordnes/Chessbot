5de3975339b46f931bfb749fd6b4aa24cf92882e
Stener Nordnes <stener.nordnes@gmail.com>
2023-12-02 21:16:19 +0100

    first init

---
+++ stockChessBot.py
@@ -0,0 +1,292 @@
+import stockfish as st
+import pyautogui as p
+from PIL import Image
+import imagehash
+import cv2
+import numpy as np
+from pyscreeze import Box
+from skimage.metrics import structural_similarity as ssim
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import keyboard
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+
+game = st.Stockfish(r"C:\Users\stene\Downloads\stockfish-windows-x86-64\stockfish\stockfish-windows-x86-64.exe", depth=18, parameters={"Threads": 2, "Minimum Thinking Time": 30})
+
+BOARD_IMG = './bot_assets/board.png'
+
+PIECE_NAME = {
+    0:'_',
+    1:'P',
+    2:'R',
+    3:'N',
+    4:'B',
+    5:'Q',
+    6:'K',
+    7:'p',
+    8:'r',
+    9:'n',
+    10:'b',
+    11:'q',
+    12:'k'
+}
+
+
+piece_mapping = {
+    'br': 'r', 'bn': 'n', 'bb': 'b', 'bk': 'k', 'bq': 'q', 'bp': 'p',
+    'wr': 'R', 'wn': 'N', 'wb': 'B', 'wk': 'K', 'wq': 'Q', 'wp': 'P'
+}
+
+
+
+
+
+
+
+def convertMoveString(moveString):
+    char_list = list(moveString)
+
+    char_list[0] = int(ord(char_list[0]) - ord('a'))
+    char_list[1] = 8 - int(char_list[1])
+    char_list[2] = int(ord(char_list[2]) - ord('a'))
+    char_list[3] = 8 - int(char_list[3])
+    return [char_list[1], char_list[0], char_list[3], char_list[2]]
+
+
+def convertMoveStringHTML(moveString):
+    char_list = list(moveString)
+
+    char_list[0] = int(ord(char_list[0]) - ord('a'))
+    char_list[1] = 8 - int(char_list[1])
+    char_list[2] = int(ord(char_list[2]) - ord('a'))
+    char_list[3] = 8 - int(char_list[3])
+    return [char_list[0], char_list[1], char_list[2], char_list[3]]
+
+
+
+
+
+class BoardVisual():
+    def __init__(self, boundingBox) -> None:
+        self.height = boundingBox.height
+        self.width =  boundingBox.width
+        self.left =  boundingBox.left
+        self.top = boundingBox.top
+
+        self.pos_array = []
+        self.piece_array = np.zeros([8,8], dtype=str)
+
+        self.Wdx = boundingBox.width//8
+        self.Hdx = boundingBox.height//8
+
+        tempArray = []
+
+        for i in range(8):
+            tempArray = []
+            for j in range(8):
+                tempArray.append((j*self.Wdx + boundingBox.left, i*self.Hdx + boundingBox.top))
+
+            self.pos_array.append(tempArray)
+
+    def get_fenString(self, turn):
+        fen = ''
+        for row in self.piece_array:
+            empty_count = 0
+            for square in row:
+                if square == '_':
+                    empty_count += 1
+                else:
+                    if empty_count > 0:
+                        fen += str(empty_count)
+                        empty_count = 0
+                    fen += square
+            if empty_count > 0:
+                fen += str(empty_count)
+            fen += '/'
+
+        # Remove the trailing '/'
+        fen = fen[:-1]
+
+        return fen +' '+ turn
+
+    def screenTiles(self,name,x,y):
+        p.screenshot(name, (self.pos_array[x][y][0],self.pos_array[x][y][1], self.Wdx, self.Hdx)) #left, top, width, height
+
+    def move_piece(self, x, y, target_x, target_y):
+
+        centerX = self.pos_array[x][y][0] + self.Wdx//2
+        centerY = self.pos_array[x][y][1] + self.Hdx//2
+
+        print(centerX, centerY)
+
+        p.click(centerX, centerY)
+
+        targetCenterX = self.pos_array[target_x][target_y][0] + self.Wdx//2
+        targetCenterY = self.pos_array[target_x][target_y][1] + self.Hdx//2
+
+        p.click(targetCenterX,targetCenterY) 
+        p.moveTo(10,10)        
+
+    def identify_tile(self):
+        # Load images
+        temp_image = cv2.imread('temp_tile.png', cv2.IMREAD_GRAYSCALE)
+        similarity_array = np.zeros(12)
+
+        # Apply Canny edge detection to extract outlines
+        temp_edges = cv2.Canny(temp_image, 50, 150)
+
+        for i in range(12):
+            baseline_image = cv2.imread(f'./bot_assets/{i+1}.png', cv2.IMREAD_GRAYSCALE)
+            
+            # Apply Canny edge detection to the baseline image
+            baseline_edges = cv2.Canny(baseline_image, 50, 150)
+
+            # Compare outlines using SSIM
+            similarity = ssim(temp_edges, baseline_edges)
+            similarity_array[i] = similarity
+
+
+        if similarity_array.max() <=0.7:
+            return 0
+        else:
+            return similarity_array.argmax() +1
+
+
+    def initialize_images(self):
+        return
+        for i in range(8):
+            for j in range(8):
+                self.screenTiles(f'./bot_assets/{i}{j}.png', i,j)
+
+    def fill_pieces(self):
+        for i in range(8):
+            for j in range(8):
+                self.screenTiles('temp_tile.png',i,j)
+                pieceNum = int(self.identify_tile())
+                self.piece_array[i,j] = PIECE_NAME[pieceNum]
+                
+
+class BoardHTML(webdriver.Chrome):
+    def __init__(self):
+        super(BoardHTML, self).__init__()
+
+        self.position = {}
+        self.size = {}
+
+    def findBoard(self):
+        svg_element = self.find_element(By.CLASS_NAME,"coordinates")
+        self.location = svg_element.location
+        self.size = svg_element.size
+
+
+    def getBoardAsFen(self, turn):
+        elements = self.find_elements(By.CSS_SELECTOR, "div[class*=piece]")
+
+        elements = elements[2:-1]
+
+        b = [['_' for _ in range(8)] for _ in range(8)]
+
+
+
+        for element in elements:
+            attr = element.get_attribute("class")
+            _, piece, pos = attr.split()
+            _,pos = pos.split('-')
+            pos = list(pos)
+            x = int(pos[0])-1
+            y = 8 - int(pos[1])
+
+            b[y][x] = piece_mapping[piece]
+
+        fen = ''
+        for row in b:
+            empty_count = 0
+            for square in row:
+                if square == '_':
+                    empty_count += 1
+                else:
+                    if empty_count > 0:
+                        fen += str(empty_count)
+                        empty_count = 0
+                    fen += square
+            if empty_count > 0:
+                fen += str(empty_count)
+            fen += '/'
+
+        # Remove the trailing '/'
+        fen = fen[:-1]
+
+        return f'{fen} {turn} KQkq - 0 1'
+
+    def movePiece(self,x, y, target_x, target_y):
+
+        self.findBoard()
+
+        Wdx = self.size['width']/8
+        Hdx = self.size['height']/8
+        centerX = self.location['x'] + Wdx/2 + x*Wdx
+        centerY = self.location['y'] + Hdx/2 + y*Hdx
+
+
+        targetCenter_x = self.location['x'] + Wdx/2 + target_x*Wdx
+        targetCenter_y = self.location['y'] + Hdx/2 + target_y*Hdx
+
+        print(targetCenter_x, targetCenter_y)
+
+
+        actions = ActionChains(self, duration=100)
+        actions.move_by_offset(centerX, centerY)
+        actions.click()
+        actions.move_by_offset(-centerX, -centerY)
+        actions.move_by_offset(targetCenter_x, targetCenter_y)
+        actions.click()
+        actions.move_by_offset(-targetCenter_x, -targetCenter_y)
+        actions.perform()
+
+
+board = BoardHTML()
+
+# Load the web page
+board.get("https://www.chess.com/play/computer")
+game.set_elo_rating(2000)
+
+
+turn = input('ready')
+board.findBoard()
+
+
+while True:
+    try:
+        if keyboard.is_pressed('r'):
+            print("Key 'R' is currently pressed.")
+            fen = board.getBoardAsFen(turn)
+            game.set_fen_position(fen)
+            print(game.get_board_visual())
+            board.save_screenshot('boardscreen.png')
+            movestring = game.get_best_move_time(1000)
+            print(movestring)
+            print(game.get_evaluation())
+            bestmove = convertMoveStringHTML(movestring)
+            print(bestmove)
+            board.movePiece(*bestmove)
+
+    except st.models.StockfishException as e:
+        game = st.Stockfish(r"C:\Users\stene\Downloads\stockfish-windows-x86-64\stockfish\stockfish-windows-x86-64.exe", depth=18, parameters={"Threads": 2, "Minimum Thinking Time": 30})
+        print('Stockfish restarted')
+    except KeyboardInterrupt:
+        break
+
+
+
+# Close the browser window
+board.quit()
+
+
+
+
+
+
+
